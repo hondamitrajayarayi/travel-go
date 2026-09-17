804a22fe2e297b25a120bc7f72c5f0c65ba82822
Rayi Detriawan <rayi.detriawan@hondamitrajaya.com>
2026-09-17 21:27:43 +0700

    fixing

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Panel;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
